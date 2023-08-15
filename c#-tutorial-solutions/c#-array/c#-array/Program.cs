feeec75a48b3da7974dc8db2013ec2506646ad88
IndieDev <tblyduc2412@gmail.com>
2023-08-15 16:09:44 +0700

    c#-tutorial-solutions: c#-array

---
+++ c#-tutorial-solutions/c#-array/c#-array/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[] items)
+{
+    string log = "";
+    foreach (var item in items)
+    {
+        log += item.ToString() + ", ";
+    }
+    log = log.Substring(0, log.Length - 2);
+    Console.WriteLine($"[{log}]");
+}
+
+int[] scores = { 3, 2, 5, 4, 1, 6 };
+
+Console.WriteLine("Array Indeces");
+Console.WriteLine($"scores[2] {scores[2]}");
+Console.WriteLine($"scores[^2] {scores[^2]}");
+Index index = 5;
+Console.WriteLine($"scores[index] {scores[index]}");
+
+
+// range returns a new array
+Console.WriteLine("\nArray Ranges");
+Console.Write("scores[2..] ");
+PrintArray(scores[2..]);
+Console.Write("scores[..2] ");
+PrintArray(scores[..2]);
+Console.Write("scores[2..5] ");
+PrintArray(scores[2..5]);
+
+Range range = 1..3;
+Console.Write("Range range = 1..3; scores[range] ");
+PrintArray(scores[range]);
---
+++ c#-tutorial-solutions/c#-array/multidimensional-arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿// a two-dimensional array with two rows and three columns
+int[,] matrix = new int[2, 3] {
+    {1, 2, 3},
+    {4, 5, 6},
+};
+
+int[,] matrix2 = {
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+};
+
+int[,,] tensor = new int[2, 2, 3] {
+    { { 1, 2, 3 }, { 4, 5, 6 } },
+    { { 7, 8, 9 }, { 10, 11, 12 } }
+};
+
+Console.WriteLine(matrix[1, 1]);
+Console.WriteLine(tensor[1, 1, 2]);
+Console.WriteLine(matrix2.Length);
+Console.WriteLine(tensor.Length);
+Console.WriteLine(tensor.GetLength(2));
+
+
+for(int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]}");
+        Console.Write(j < matrix.GetLength(1) - 1 ? "," : "");
+    }
+    Console.WriteLine();
+}
