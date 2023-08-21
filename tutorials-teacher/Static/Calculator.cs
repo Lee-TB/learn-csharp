127723843ddfc2293264f1e6f9a4da259572520f
IndieDev <tblyduc2412@gmail.com>
2023-08-21 16:21:09 +0700

    tutorials-teacher: static class, static method, static constructor.

---
+++ tutorials-teacher/Static/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal static class Calculator
+    {
+        private static int _resultStorage = 0;
+        public static string Type = "Arithmetic";
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static void Store(int result)
+        {
+            _resultStorage = result;
+        }
+    }
+}
---
+++ tutorials-teacher/Static/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw1 = new StopWatch();
+            var sw2 = new StopWatch();
+        }
+    }
+}
+
+public class StopWatch
+{
+    // static constructor
+    static StopWatch()
+    {
+        Console.WriteLine("Static constructor called");
+    }
+
+    // instance constructor
+    public StopWatch()
+    {
+        Console.WriteLine("Instance constructor called");
+    }
+
+    // static method
+    public static void DisplayInfo()
+    {
+        Console.WriteLine("DisplayInfo called");
+    }
+
+    // instance method
+    public void Start() { }
+
+    // instance method
+    public void Stop() { }
+}
