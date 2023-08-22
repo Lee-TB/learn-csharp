b998f283e9f0e90cf90a9932f3ec06397539c437
IndieDev <tblyduc2412@gmail.com>
2023-08-22 12:54:07 +0700

    tutorials-teacher: Generics

---
+++ tutorials-teacher/Generics/KeyValuePair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class KeyValuePair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+}
---
+++ tutorials-teacher/Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics
+{
+    internal class Program
+    {        
+        static void Main()
+        {            
+            Example1();
+        }
+
+        static void Example1()
+        {
+            DataStore<string> strStore = new DataStore<string>();
+            strStore.Data = "Hello world!";
+            Console.WriteLine(strStore.Data);
+            //strStore.Data = 100; // compile-time error
+
+            DataStore<int> intStore = new DataStore<int>();
+            intStore.Data = 100;
+            Console.WriteLine(intStore.Data);
+
+            KeyValuePair<int, string> kvp1 = new KeyValuePair<int, string>();
+            kvp1.Key = 1;
+            kvp1.Value = "C#";
+            Console.WriteLine(kvp1.Key + " " + kvp1.Value);
+
+            KeyValuePair<string, string> kvp2 = new KeyValuePair<string, string>();
+            kvp2.Key = "IT";
+            kvp2.Value = "Information Technology";
+            Console.WriteLine(kvp2.Key + " " + kvp2.Value);
+        }
+    }
+}
