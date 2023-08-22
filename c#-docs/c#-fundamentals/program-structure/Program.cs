a0a777bab60d8ccfa930bd84924c76dd86d1f5cc
IndieDev <tblyduc2412@gmail.com>
2023-08-22 13:29:11 +0700

    c#-fundamentals: program-structure

---
+++ c#-docs/c#-fundamentals/program-structure/Program.cs
@@ -0,0 +1,74 @@
+﻿// A skeleton of a C# program
+using System;
+
+// Your program starts here:
+Console.WriteLine("Hello world!");
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+    }
+
+    struct YourStruct
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+        }
+    }
+}
+
+
+//// Prior to C# 9, the entry point was a static method named Main, as shown in the following example:
+//// A skeleton of a C# program
+//using System;
+//namespace YourNamespace
+//{
+//    class YourClass
+//    {
+//    }
+
+//    struct YourStruct
+//    {
+//    }
+
+//    interface IYourInterface
+//    {
+//    }
+
+//    delegate int YourDelegate();
+
+//    enum YourEnum
+//    {
+//    }
+
+//    namespace YourNestedNamespace
+//    {
+//        struct YourStruct
+//        {
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Your program starts here...
+//            Console.WriteLine("Hello world!");
+//        }
+//    }
+//}
