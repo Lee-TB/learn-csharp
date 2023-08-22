2e55c01a6129848ac071aa4353e76862bf9af0b1
IndieDev <tblyduc2412@gmail.com>
2023-08-22 16:04:41 +0700

    c#-fundamentals: Interfaces

---
+++ c#-docs/c#-fundamentals/interfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    interface IEquatable<T>
+    {
+        bool Equals(T obj);
+    }
+
+    class Car : IEquatable<Car> {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public string? Year { get; set; }
+
+        public bool Equals(Car? car) {
+            return (this.Make, this.Model, this.Year) == (car?.Make, car?.Model, car?.Year);
+        }
+    }
+}
