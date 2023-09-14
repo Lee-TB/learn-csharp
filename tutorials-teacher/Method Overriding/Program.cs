ce2aa15d8391f9abda600244a31c6c5edad81b78
IndieDev <tblyduc2412@gmail.com>
2023-09-14 15:38:01 +0700

    tutorials-teacher: Method Overriding

---
+++ tutorials-teacher/Method Overriding/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Method_Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Greet();
+
+            Person p2 = new Employee();
+            p2.Greet();
+
+            Employee emp = new Employee();
+            emp.Greet();
+
+            Console.WriteLine();
+            Display(p1);
+            Display(emp);
+        }
+
+        static void Display(Person p)
+        {
+            p.Greet();
+        }
+
+        class Person
+        {
+            public virtual void Greet()
+            {
+                Console.WriteLine("Hi! I'm a person");
+            }
+        }
+
+        class Employee : Person
+        {
+
+            public override void Greet()
+            {
+                Console.WriteLine("Hi! I'm a employee");
+            }
+        }
+    }
+}
