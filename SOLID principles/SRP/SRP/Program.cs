c1a19c5e79a5c0e69a6a7de3ed8047cf3af1a370
IndieDev <tblyduc2412@gmail.com>
2023-09-14 13:54:34 +0700

    SOLID: SRP

---
+++ SOLID principles/SRP/SRP/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace SRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Name { get; set; }
+    public string AuthorName { get; set; }
+    public int Year { get; set; }
+    public double Price { get; set; }
+}
+
+public class Invoice
+{
+    public Book book;
+    public int quantity;
+    public double discountRate;
+    public double taxRate;
+    public double Total
+    {
+        get
+        {
+            double price = ((book.Price - book.Price * discountRate) * this.quantity);
+            return price * (1 + taxRate);
+        }
+    }
+
+    public Invoice(Book book, int quantity, double discountRate, double taxRate)
+    {
+        this.book = book;
+        this.quantity = quantity;
+        this.discountRate = discountRate;
+        this.taxRate = taxRate;
+    }
+}
+
+public class InvoicePrinter
+{
+    private Invoice invoice;
+    public InvoicePrinter(Invoice invoice)
+    {
+        this.invoice = invoice;
+    }
+    public void Print()
+    {
+        Console.WriteLine(invoice.quantity + "x " + invoice.book.Name + " " + invoice.book.Price + " $");
+        Console.WriteLine("Discount Rate: " + invoice.discountRate);
+        Console.WriteLine("Tax Rate: " + invoice.taxRate);
+        Console.WriteLine("Total: " + invoice.Total + " $");
+    }
+}
+
+public class InvoicePersistence
+{
+    Invoice invoice;
+
+    public InvoicePersistence(Invoice invoice)
+    {
+        this.invoice = invoice;
+    }
+
+    public void saveToFile(String filename)
+    {
+        // Creates a file with given name and writes the invoice
+    }
+}
---
+++ SOLID principles/SRP/SRP/Violate.cs
@@ -0,0 +1,47 @@
+﻿namespace SRP;
+
+internal class Violate
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Invoice
+    {
+        private Book book;
+        private int quantity;
+        private double discountRate;
+        private double taxRate;
+        private double Total
+        {
+            get
+            {
+                double price = ((book.Price - book.Price * discountRate) * this.quantity);
+                return price * (1 + taxRate);
+            }
+        }
+
+        public Invoice(Book book, int quantity, double discountRate, double taxRate)
+        {
+            this.book = book;
+            this.quantity = quantity;
+            this.discountRate = discountRate;
+            this.taxRate = taxRate;
+        }
+
+        public void printInvoice()
+        {
+
+        }
+
+        public void saveToFile(String filename)
+        {
+            // Creates a file with given name and writes the invoice
+        }
+    }
+}
